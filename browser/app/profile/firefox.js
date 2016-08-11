1a096da1e527defd27ced593ae1dbb20ba013662
Georg Koppen <gk@torproject.org>
2016-08-11 11:00:24 +0000

    Bug 19890: Disable installation of system addons
    By setting the URL to "" we make sure that already installed system
    addons get deleted as well.

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -52,8 +52,8 @@ pref("extensions.getAddons.themes.browseURL", "https://addons.mozilla.org/%LOCAL
 
 pref("extensions.update.autoUpdateDefault", true);
 
-// Check AUS for system add-on updates.
-pref("extensions.systemAddon.update.url", "https://aus5.mozilla.org/update/3/SystemAddons/%VERSION%/%BUILD_ID%/%BUILD_TARGET%/%LOCALE%/%CHANNEL%/%OS_VERSION%/%DISTRIBUTION%/%DISTRIBUTION_VERSION%/update.xml");
+// No AUS check for system add-on updates for Tor Browser users.
+pref("extensions.systemAddon.update.url", "");
 
 // Disable add-ons that are not installed by the user in all scopes by default.
 // See the SCOPE constants in AddonManager.jsm for values to use here.
