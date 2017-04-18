f7439620a785dc8046eb3617f3eb9fff1e6faaa5
Zibi Braniecki <gandalf@mozilla.com>
2017-04-18 01:03:05 -0700

    Bug 1005640 - Flush StringBundle cache when app-locales change. r=valentin
    StringBundle caches bundles, so when language chain changes we should
    flush the cache to enable new strings to be loaded.
    This also affects localized prefs like intl.accept_languages.
    
    Then in HttpHandler we have to mark the value as dirty so that next
    time it's called it actually recalculates using flushed string bundle
    with the new locale.
    
    MozReview-Commit-ID: DKWEDUli4yH
    
    --HG--
    extra : rebase_source : 75ecc4204deca066d7492d1494492a91685f36be
    
    This fixes bug 22659 on our side.

--- netwerk/protocol/http/nsHttpHandler.h
+++ netwerk/protocol/http/nsHttpHandler.h
@@ -393,7 +393,7 @@ private:
     void     PrefsChanged(nsIPrefBranch *prefs, const char *pref);
 
     nsresult SetAccept(const char *);
-    nsresult SetAcceptLanguages(const char *);
+    nsresult SetAcceptLanguages();
     nsresult SetAcceptEncodings(const char *, bool mIsSecure);
 
     nsresult InitConnectionMgr();
@@ -497,6 +497,7 @@ private:
     nsCString      mUserAgent;
     nsXPIDLCString mUserAgentOverride;
     bool           mUserAgentIsDirty; // true if mUserAgent should be rebuilt
+    bool           mAcceptLanguagesIsDirty;
 
 
     bool           mPromptTempRedirect;
