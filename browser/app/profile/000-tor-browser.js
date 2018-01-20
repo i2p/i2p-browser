1c3ff5e95523af3fcde5a9a75fa6a4fa445d9777
Fernando Fernandez Mancera <ffmancera@riseup.net>
2018-01-20 21:54:00 +0100

    Bug 13575: Disable randomised Firefox HTTP cache decay user tests.
    Mozilla's experiment shouldn't be collecting any data on our users. If
    "browser.cache.frecency_experiment" is not set in firefox.js to "-1" it will
    default to "0" and then it is randomised between "1" and "4" inclusive, setting
    different HTTP cache decay times for the four groups.

--- browser/app/profile/000-tor-browser.js
+++ browser/app/profile/000-tor-browser.js
@@ -179,6 +179,9 @@ pref("media.webspeech.synth.enabled", false); // Bug 10283: Disable SpeechSynthe
 pref("dom.webaudio.enabled", false); // Bug 13017: Disable Web Audio API
 pref("dom.maxHardwareConcurrency", 1); // Bug 21675: Spoof single-core cpu
 pref("dom.w3c_touch_events.enabled", 0); // Bug 10286: Always disable Touch API
+// Disable randomised Firefox HTTP cache decay user test groups (Bug: 13575)
+pref("browser.cache.frecency_experiment", -1);
+
 
 // Third party stuff
 pref("privacy.firstparty.isolate", true); // Always enforce first party isolation
