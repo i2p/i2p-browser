084a76168f4d2c15d1adc95b6a669c61898066f9
Tim Huang <tihuang@mozilla.com>
2018-07-03 13:47:45 +0000

    Bug 1470156 - Part 2: Fixing the crashing problem when using an invalid character in a firstPartyDomain. r=baku
    This patch adds a sanitization of firstPartyDomain when calling the
    OriginAttributes::CreateSuffix() and remove the release assert there.
    The cookies API for the web extension can use a arbitrary string for the
    firstPartyDomain. So, we should sanitize the firstPartyDomain before
    we creating a suffix. The release assert is not required anymore since
    the firstPartyDomain is sanitized
    
    Depends on D1845.
    
    Differential Revision: https://phabricator.services.mozilla.com/D1856
    
    --HG--
    extra : moz-landing-system : lando

--- caps/OriginAttributes.cpp
+++ caps/OriginAttributes.cpp
@@ -143,10 +143,12 @@ void OriginAttributes::CreateSuffix(nsACString& aStr) const {
   }
 
   if (!mFirstPartyDomain.IsEmpty()) {
-    MOZ_RELEASE_ASSERT(mFirstPartyDomain.FindCharInSet(
-                           dom::quota::QuotaManager::kReplaceChars) ==
-                       kNotFound);
-    params.Set(NS_LITERAL_STRING("firstPartyDomain"), mFirstPartyDomain);
+    nsAutoString sanitizedFirstPartyDomain(mFirstPartyDomain);
+    sanitizedFirstPartyDomain.ReplaceChar(
+        dom::quota::QuotaManager::kReplaceChars, '+');
+
+    params.Set(NS_LITERAL_STRING("firstPartyDomain"),
+               sanitizedFirstPartyDomain);
   }
 
   aStr.Truncate();
