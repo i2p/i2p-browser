cb63b9e84081738dc48df125021aa8d1ce06e6f4
Matthew Finkel <Matthew.Finkel@gmail.com>
2018-09-05 14:00:01 +0000

    Bug 27473 - Change app name on Android for Alpha version

--- mobile/android/branding/torbrowser/configure.sh
+++ mobile/android/branding/torbrowser/configure.sh
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-ANDROID_PACKAGE_NAME=org.torproject.torbrowser
-MOZ_APP_DISPLAYNAME="Tor Browser for Android"
+ANDROID_PACKAGE_NAME=org.torproject.torbrowser_alpha
+MOZ_APP_DISPLAYNAME="Tor Browser for Android (Alpha)"
 MOZ_UPDATER=
 MOZ_ANDROID_ANR_REPORTER=
