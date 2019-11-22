379a29244bb15c202eb786ba303f10bb434cff8f
Mikal Villa <mikalv@mikalv.net>
2019-11-22 17:46:57 +0100

    Disable WebGL readPixel() for web content

--- dom/canvas/WebGLContextGL.cpp
+++ dom/canvas/WebGLContextGL.cpp
@@ -1189,6 +1189,14 @@ bool WebGLContext::ReadPixels_SharedPrecheck(CallerType aCallerType,
     out_error.Throw(NS_ERROR_DOM_SECURITY_ERR);
     return false;
   }
+  // Security check passed, but don't let content readPixel calls through for
+  // now, if Resist Fingerprinting Mode is enabled.
+  if (nsContentUtils::ResistFingerprinting(aCallerType)) {
+    GenerateWarning("readPixels: Not allowed in Resist Fingerprinting Mode");
+    out_error.Throw(NS_ERROR_DOM_NOT_SUPPORTED_ERR);
+    return false;
+  }
+
 
   return true;
 }
