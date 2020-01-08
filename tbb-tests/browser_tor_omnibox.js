arthuredelstein/tor-browser

Regression tests for "Omnibox: Add DDG, Startpage, Disconnect, Youtube, Twitter; remove Amazon, eBay, bing"

tbb-tests/browser_tor_omnibox.js
@@ -1,0 +1,14 @@
+// # Test Tor Omnibox
+// Check what search engines are installed in the search box.
+
+function test() {
+  // Grab engine IDs.
+  let browserSearchService = Components.classes["@mozilla.org/browser/search-service;1"]
+                             .getService(Components.interfaces.nsIBrowserSearchService),
+      engineIDs = browserSearchService.getEngines().map(e => e.identifier);
+
+  // Check that we have the correct engines installed, in the right order.
+  is(engineIDs[0], "disconnect", "Default search engine is disconnect");
+  is(engineIDs[1], "youtube", "Secondary search engine is youtube");
+  is(engineIDs[2], "google", "Google is third search engine");
+}

i2p/i2p-browser

tbb-tests/browser_tor_omnibox.js
@@ -1,0 +1,14 @@
+// # Test Tor Omnibox
+// Check what search engines are installed in the search box.
+
+function test() {
+  // Grab engine IDs.
+  let browserSearchService = Components.classes["@mozilla.org/browser/search-service;1"]
+                             .getService(Components.interfaces.nsIBrowserSearchService),
+      engineIDs = browserSearchService.getEngines().map(e => e.identifier);
+
+  // Check that we have the correct engines installed, in the right order.
+  is(engineIDs[0], "ddg", "Default search engine is duckduckgo");
+  is(engineIDs[1], "youtube", "Secondary search engine is youtube");
+  is(engineIDs[2], "google", "Google is third search engine");
+}